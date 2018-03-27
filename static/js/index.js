5d7119e162606864c1d8486a0730e8943bb75f5a
kletteraffe11 <s.couball@gmail.com>
2018-03-27 15:11:56 +0200

    css cleanup added favicon

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,3 @@
-// external js: masonry.pkgd.js, imagesloaded.pkgd.js
-
 var $grid = $('.grid').imagesLoaded( function() {
     // init Masonry after all images have loaded
     $grid.masonry({
@@ -22,18 +20,6 @@ $(document).ready(function(){
     });
 });
 
-$(window).resize(function () {
-    var viewportWidth = $(window).width();
-    if (viewportWidth < 641) {
-            $(".meta").addClass("grid-item--width2");
-    }
-});
-$(window).resize(function () {
-    var viewportWidth = $(window).width();
-    if (viewportWidth > 641) {
-            $(".meta").removeClass("grid-item--width2");
-    }
-});
 $(window).imagesLoaded(function () {
     var viewportWidth = $(window).width();
     if (viewportWidth < 641) {
